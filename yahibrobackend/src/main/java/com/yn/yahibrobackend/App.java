e057022058f5e3397ef16c51438f9f260cc04988
Yahia Nurhussien <yahianurhussien@gmail.com>
2019-03-05 14:44:21 -0800

    My project setup completed successfully

---
+++ yahibrobackend/src/main/java/com/yn/yahibrobackend/App.java
@@ -0,0 +1,13 @@
+package com.yn.yahibrobackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
